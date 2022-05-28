8962bccf22040882eafb0b872df90e1bd4ad2037
Jsmond2016 <Jsmond2016@gmail.com>
2022-05-28 22:27:55 +0800

    feat: 配置 jest-enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme'
+import Adapater from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapater() })
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('[data-test="container"]')).toExist()
+})
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
